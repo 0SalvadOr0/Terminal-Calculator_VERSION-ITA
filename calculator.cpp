d9452d978a049d27b1557aa48d8f4eb2b79ff28d
0SalvadOr0 <grizzzaffigiovy219@gmail.com>
2023-08-25 12:40:25 +0200

    Refactoring

--- calculator.cpp
+++ calculator.cpp
@@ -125,62 +125,6 @@ void choice() {
 }
 
 
-void addition(float &a, float &b) {
-    std::cout << "Inserisci primo valore: ";
-    std::cin >> a;
-    std::cout << "Inserisci secondo valore: ";
-    std::cin >> b;
-    std::cout << "Risultato dell'addizione: " << a + b << std::endl;
-}
-
-void subtraction(float &a, float &b) {
-    std::cout << "Inserisci primo valore: ";
-    std::cin >> a;
-    std::cout << "Inserisci secondo valore: ";
-    std::cin >> b;
-    std::cout << "Risultato della sottrazione: " << a - b << std::endl;
-}
-
-void multiplication(float &a, float &b) {
-    std::cout << "Inserisci primo valore: ";
-    std::cin >> a;
-    std::cout << "Inserisci secondo valore: ";
-    std::cin >> b;
-    std::cout << "Risultato della moltiplicazione: " << a * b << std::endl;
-}
-
-void division(float &a, float &b) {
-    std::cout << "Inserisci primo valore: ";
-    std::cin >> a;
-    std::cout << "Inserisci secondo valore: ";
-    std::cin >> b;
-
-    if (b == 0) {
-        std::cout << "Divisione per zero non consentita." << std::endl;
-    } else {
-        std::cout << "Risultato della divisione: " << a / b << std::endl;
-    }
-}
-
-void Pow(float &a, float &b) {
-    std::cout << "Inserisci il primo valore: ";
-    std::cin >> a;
-    std::cout << "Inserisci il valore della potenza: ";
-    std::cin >> b;
-    std::cout << "Risultato finale: " << std::pow(a, b) << std::endl;
-}
-
-void Sqrt(float &a) {
-    std::cout << "Inserisci il valore: ";
-    std::cin >> a;
-    
-    if (a < 0) {
-        std::cout << "Valore negativo, impossibile calcolare la radice quadrata." << std::endl;
-    } else {
-        std::cout << "Risultato finale: " << std::sqrt(a) << std::endl;
-    }
-}
-
 void Triangle() {
     int t_option;
     std::cout << "Scegli l'opzione:\n 1. Calcola il cateto\n 2. Calcola l'ipotenusa\n";
---
+++ operation/operation.h
@@ -0,0 +1,16 @@
+#pragma once 
+
+#ifndef OPERATION_H
+#define OPERATION_H
+
+#include <iostream>
+#include <math.h>
+
+void addition(float &a, float &b);
+void subtraction(float &a, float &b);
+void multiplication(float &a, float &b);
+void division(float &a, float &b);
+void Pow(float &a, float &b);
+void Sqrt(float &a);
+
+#endif
