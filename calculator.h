548cdabf22e63cd28dd45e8215584829d257d7d2
0SalvadOr0 <grizzzaffigiovy219@gmail.com>
2023-08-25 12:40:08 +0200

    Refactoring

--- calculator.h
+++ calculator.h
@@ -12,12 +12,6 @@
 
 
 void choice();
-void addition(float &a, float &b);
-void subtraction(float &a, float &b);
-void multiplication(float &a, float &b);
-void division(float &a, float &b);
-void Pow(float &a, float &b);
-void Sqrt(float &a);
 void Triangle();
 void TrigonometricOperations();
 void PercentageCalculations();
