58d3ccb97b8e050d316436466acc50c2710e06a1
0SalvadOr0 <grizzzaffigiovy219@gmail.com>
2023-09-04 11:21:44 +0200

    Refac

--- headers/calculator.cpp
+++ headers/calculator.cpp
@@ -145,66 +145,6 @@ void choice() {
 }
 
 
-void Triangle() {
-    int t_option;
-    std::cout << "Scegli l'opzione:\n 1. Calcola il cateto\n 2. Calcola l'ipotenusa\n";
-    std::cin >> t_option;
-    
-    float a, b, c;
-    
-    switch (t_option) {
-        case 1:
-            std::cout << "Inserisci il valore dell'ipotenusa: ";
-            std::cin >> a;
-            std::cout << "Inserisci il valore del cateto: ";
-            std::cin >> b;
-            c = std::sqrt(pow(a, 2) - pow(b, 2));
-            std::cout << "Il cateto misura: " << c << std::endl;
-            break;
-        case 2:
-            std::cout << "Inserisci il valore del cateto maggiore: ";
-            std::cin >> a;
-            std::cout << "Inserisci il valore del cateto minore: ";
-            std::cin >> b;
-            c = std::sqrt(pow(a, 2) + pow(b, 2));
-            std::cout << "Il cateto misura: " << c << std::endl;
-            break;
-        default:
-            std::cout << "Opzione non valida" << std::endl;
-            break;
-    }
-}
-
-void PercentageCalculations() {
-    int choice;
-    float initialValue, percentage, result;
-
-    std::cout << "Scegli l'operazione:\n 1. Calcola la variazione percentuale\n 2. Calcola lo sconto\n";
-    std::cin >> choice;
-
-    switch (choice) {
-        case 1:
-            std::cout << "Inserisci il valore iniziale: ";
-            std::cin >> initialValue;
-            std::cout << "Inserisci la percentuale di variazione: ";
-            std::cin >> percentage;
-            result = initialValue * (percentage / 100);
-            std::cout << "Variazione percentuale: " << result << std::endl;
-            break;
-        case 2:
-            std::cout << "Inserisci il prezzo originale: ";
-            std::cin >> initialValue;
-            std::cout << "Inserisci lo sconto in percentuale: ";
-            std::cin >> percentage;
-            result = initialValue - (initialValue * (percentage / 100));
-            std::cout << "Prezzo dopo lo sconto: " << result << std::endl;
-            break;
-        default:
-            std::cout << "Opzione non valida.\n";
-            break;
-    }
-}
-
 
 void BMICalculator() {
     float weight, height;
